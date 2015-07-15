edb19f0e78153f7fc1634785a0c31278573297ce
Adam Caron <adamwcaron@gmail.com>
2015-07-14 18:05:11 -0600

    adds 'statuses' table to db; adds 'status' model; creates relationships between 'status' and 'task'

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   # this task takes care of both the instances of the class, and the collection so it's not necessary to have a separate TaskManager class.
   belongs_to :user
+  belongs_to :status
   # belongs_to :status
   validates_presence_of :title # same as `validates :title, presence: true` except the former syntax validates multiple attributes. The latter validates multiple things (title is present, has the correct length etc.) about one attribue (for instance, just the title)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713205436) do
+ActiveRecord::Schema.define(version: 20150714234640) do
+
+  create_table "statuses", force: :cascade do |t|
+    t.text "name"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.text     "title"
@@ -19,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150713205436) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "status_id"
   end
 
   create_table "users", force: :cascade do |t|
